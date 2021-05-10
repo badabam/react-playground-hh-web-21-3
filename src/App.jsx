ea64f6af93d41f3a8ae4b6c3fee2763942ac2746
Jeremias Erbs <jerry@badabam.de>
2021-05-10 16:55:17 +0200

    Improve Button component

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Header from './Header'
 export default () => {
   return (
     <div>
-      <Header isBackButtonVisible />
-      <Header />
+      <Header isBackButtonVisible>Header with Button</Header>
+      <Header>Another Header</Header>
     </div>
   )
 }
