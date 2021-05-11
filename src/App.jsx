ced5c9f921630775935db59588841076fc2b84a4
Jeremias Erbs <jerry@badabam.de>
2021-05-11 15:20:45 +0200

    Add an array in App.jsx to create Cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,27 @@ import * as React from 'react'
 import Card from './Card'
 import './App.css'
 
+const cards = [
+  { title: 'Hello world', text: 'How are you?', isBookmarked: false, id: 1 },
+  {
+    title: 'Hi again',
+    text: 'I am great, thank you',
+    isBookmarked: true,
+    id: 2,
+  },
+]
+
 export default () => {
   return (
     <div className="App">
-      <Card title="Hello world" copyText="How are you?" />
-      <Card isBookmarked title="Hi again" copyText="I am great" />
+      {cards.map(({ id, title, text, isBookmarked }) => (
+        <Card
+          key={id}
+          title={title}
+          copyText={text}
+          isBookmarked={isBookmarked}
+        />
+      ))}
     </div>
   )
 }
