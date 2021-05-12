2da85770f81babebff985bfd686b47b4612759b4
Jeremias Erbs <jerry@badabam.de>
2021-05-12 09:22:21 +0200

    Implement Cards with data from Open Trivia DB

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
 import * as React from 'react'
 import Card from './Card'
 import './App.css'
+import { decode } from 'html-entities'
+import { v4 as uuidv4 } from 'uuid'
 
-const cards = [
-  { title: 'Hello world', text: 'How are you?', isBookmarked: false, id: 1 },
-  {
-    title: 'Hi again',
-    text: 'I am great, thank you',
-    isBookmarked: true,
-    id: 2,
-  },
-]
+import data from './data.json'
+
+const cards = data.map(item => ({
+  id: uuidv4(),
+  title: item.category,
+  question: decode(item.question),
+  choices: item.incorrect_answers
+    .concat(item.correct_answer)
+    .map(answer => ({ sort: Math.random(), value: decode(answer) }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(item => item.value),
+  answer: decode(item.correct_answer),
+  isBookmarked: Math.random() > 0.5,
+  doShowAnswer: Math.random() > 0.5,
+}))
 
 export default () => {
   return (
     <div className="App">
-      {cards.map(({ id, title, text, isBookmarked }) => (
-        <Card
-          key={id}
-          title={title}
-          copyText={text}
-          isBookmarked={isBookmarked}
-        />
-      ))}
+      {cards.map(
+        ({
+          id,
+          title,
+          question,
+          choices,
+          answer,
+          isBookmarked,
+          doShowAnswer,
+        }) => (
+          <Card
+            key={id}
+            title={title}
+            question={question}
+            choices={choices}
+            answer={answer}
+            isBookmarked={isBookmarked}
+            doShowAnswer={doShowAnswer}
+          />
+        )
+      )}
     </div>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,14 @@
 import * as React from 'react'
 import './Card.css'
 
-export default function Card({ title, copyText, isBookmarked }) {
+export default function Card({
+  title,
+  question,
+  choices,
+  answer,
+  isBookmarked,
+  doShowAnswer,
+}) {
   const bookmarkClass = isBookmarked
     ? 'Card__bookmark Card__bookmark--active'
     : 'Card__bookmark'
@@ -10,7 +17,19 @@ export default function Card({ title, copyText, isBookmarked }) {
     <section className="Card">
       <button className={bookmarkClass} aria-label="Bookmark card"></button>
       <h2 className="Card__title">{title}</h2>
-      <p>{copyText}</p>
+      <dl>
+        <dt className="Card__question">
+          🧐 {question}
+          <ul className="Card__choices">
+            {choices.map(choice => (
+              <li key={choice}>{choice}</li>
+            ))}
+          </ul>
+        </dt>
+        <dd className="Card__answer" hidden={!doShowAnswer}>
+          🤓 {answer}
+        </dd>
+      </dl>
     </section>
   )
 }
