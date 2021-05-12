55557dd314a2573068512bc8fcd9ed06c6a8b76c
Jeremias Erbs <jerry@badabam.de>
2021-05-12 16:06:43 +0200

    Add state for hue (in addition to size)

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
 import React, { useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
 import './App.css'
 
 export default () => {
-  const [size, setSize] = useState(100)
+  const [size, setSize] = useState('100')
+  const [hue, setHue] = useState('0')
 
   const style = {
     width: size + 'px',
     height: size + 'px',
+    backgroundColor: `hsl(${hue}deg, 70%, 70%)`,
   }
 
   return (
     <div className="App">
       <label>
-        Size:
-        <input value={size} onChange={handleChange} type="range" max="200" />
+        Size:{' '}
+        <input
+          value={size}
+          onChange={handleSizeChange}
+          type="range"
+          max="200"
+        />
+      </label>
+      <label>
+        Color:{' '}
+        <input value={hue} onChange={handleHueChange} type="range" max="359" />
       </label>
       <div style={style} className="Box" />
     </div>
   )
 
-  function handleChange(event) {
+  function handleSizeChange(event) {
     const input = event.target
     setSize(input.value)
+  }
+
+  function handleHueChange(event) {
+    const input = event.target
+    setHue(input.value)
   }
 }
